d40c10b61910396011e1dfe35cf160f1f683d763
KripakDebug <kastrula123@gmail.com>
2024-02-26 18:32:05 +0200

    fix pretter

--- src/pages/Auth/Auth.jsx
+++ src/pages/Auth/Auth.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import React from "react";
 
 function Auth() {
 	return (
-		<div className='flex h-screen flex-col-reverse bg-[#181818] text-white md:flex-row'>
-			<div className='br-100 flex w-full items-center justify-center bg-[#24a1e0] md:w-3/6 md:rounded-r-[150px] '>
-				<div className='flex flex-col items-center justify-center'>
+		<div className="flex h-screen flex-col-reverse bg-[#181818] text-white md:flex-row">
+			<div className="br-100 flex w-full items-center justify-center bg-[#24a1e0] md:w-3/6 md:rounded-r-[150px] ">
+				<div className="flex flex-col items-center justify-center">
 					<div className=" text-center font-['Nunito_Sans'] text-[40px] font-bold">
 						Добро пожаловать
 					</div>
@@ -15,7 +15,7 @@ function Auth() {
 					<button></button>
 				</div>
 			</div>
-			<div className='flex w-full items-center justify-center md:w-3/6'>
+			<div className="flex w-full items-center justify-center md:w-3/6">
 				lorem150rerere
 			</div>
 		</div>
