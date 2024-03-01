3b50fc9630c9cea7e40493cec98ac79bd5f2bb85
KripakDebug <kastrula123@gmail.com>
2024-03-02 01:23:13 +0200

    create login page and register page

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,11 +1,54 @@
-import React from 'react';
+import { Form } from "antd";
+import React, { useState } from "react";
+import "react-international-phone/style.css";
+import { isPhoneValid } from "../../utils/helpers";
+import FormRegisterPartOne from "./FormRegisterPartOne";
+import FormRegisterPartTwo from "./FormRegisterPartTwo";
 
-function Register() {
-    return (
-        <div>
-            
-        </div>
-    );
+export default function Register() {
+	const [phone, setPhone] = useState("");
+	const isValid = isPhoneValid(phone);
+	const [showMessagewithErrorPhone, setShowMessagewithErrorPhone] =
+		useState(false);
+	const [formChange, setFormChange] = useState(true);
+	return (
+		<Form
+			className="w-3/5 text-white max-md:w-full"
+			initialValues={{
+				remember: true,
+			}}
+			onFinish={onFinish}
+			autoComplete="off"
+		>
+			<div className="mb-[25px] text-center text-[40px] font-bold">
+				Создание аккаунта
+			</div>
+			{formChange ? (
+				<FormRegisterPartOne
+					formChange={formChange}
+					phone={phone}
+					setPhone={setPhone}
+					setFormChange={setFormChange}
+					showMessagewithErrorPhone={showMessagewithErrorPhone}
+					checkedValidatePhoneNumber={checkedValidatePhoneNumber}
+				/>
+			) : (
+				<FormRegisterPartTwo
+					setFormChange={setFormChange}
+					formChange={formChange}
+				/>
+			)}
+		</Form>
+	);
+
+	function onFinish(values) {
+		console.log("Success:", values);
+	}
+	function checkedValidatePhoneNumber() {
+		if (!isValid) {
+			setShowMessagewithErrorPhone(true);
+			return;
+		}
+		setShowMessagewithErrorPhone(false);
+	}
 }
-
-export default Register;
--- src/pages/Auth/Auth.jsx
+++ src/pages/Auth/Auth.jsx
@@ -1,25 +1,79 @@
-import React from "react";
+import React, { useState } from "react";
+import Register from "../../components/Register/Register";
+import { NavLink, useNavigate } from "react-router-dom";
+import Login from "../../components/Login/Login";
+import { ConfigProvider, theme } from "antd";
 
 function Auth() {
+	const navigate = useNavigate();
+
+	const [isActive, setIsActive] = useState(
+		window.location.pathname === "/signup",
+	);
+
 	return (
-		<div className="flex h-screen flex-col-reverse bg-[#181818] text-white md:flex-row">
-			<div className="br-100 flex w-full items-center justify-center bg-[#24a1e0] md:w-3/6 md:rounded-r-[150px] ">
+		<div
+			className={`relative flex min-h-screen flex-col-reverse bg-[#181818] text-white lg:flex-row ${!isActive && "lg:flex-row-reverse"}`}
+		>
+			<div
+				className={`flex w-3/6 items-center justify-center bg-[#24a1e0] p-[20px] max-lg:w-full max-lg:rounded-t-[100px] lg:rounded-r-[150px] ${!isActive && "lg:rounded-l-[150px] lg:rounded-r-[0px]"}`}
+			>
 				<div className="flex flex-col items-center justify-center">
-					<div className=" text-center font-['Nunito_Sans'] text-[40px] font-bold">
-						Добро пожаловать
+					<div className=" text-center text-[40px] font-bold max-md:text-[30px]">
+						{isActive ? "Добро пожаловать" : "С возвращением!"}
 					</div>
-					<div className="w-[400px] text-center font-['Nunito_Sans'] text-[20px] font-normal">
-						Заполните поля что бы пользоваться всеми функциями AiPro Если у вас
-						уже есть аккаунт, вы можете войти в него
+					<div className="w-[400px] text-center text-[20px] font-normal max-md:w-auto max-md:text-[16px]">
+						Заполните поля что бы пользоваться всеми функциями AiPro
+						<p>
+							{isActive
+								? "Если у вас уже есть аккаунт, вы можете войти в него"
+								: "Если у вас еще нет аккаунта, вы можете создать его"}
+						</p>
 					</div>
-					<button></button>
+					<button
+						className=" mt-[15px]  rounded-[10px] border border-solid p-[5px] px-[30px] text-[20px] hover:bg-[#2faeed]"
+						onClick={isActive ? handleLoginClick : handleRegisterClick}
+					>
+						{isActive ? "Войти" : "Зарегистрироваться"}
+					</button>
 				</div>
 			</div>
-			<div className="flex w-full items-center justify-center md:w-3/6">
-				lorem150rerere
+			<div className="flex w-full items-center justify-center p-[20px] lg:w-3/6">
+				<ConfigProvider
+					theme={{
+						token: {
+							algorithm: theme.darkAlgorithm,
+						},
+						components: {
+							Input: {
+								colorBgContainer: "none",
+								colorBorder: "rgb(63, 63, 63)",
+								colorIcon: "#fff",
+								colorTextPlaceholder: "rgb(180, 180, 180)",
+								colorText: "rgb(254, 254, 254)",
+								paddingBlock: "10px",
+								fontSize: "17px",
+								activeBorderColor: "grey",
+								hoverBorderColor: "grey",
+							},
+						},
+					}}
+				>
+					{isActive ? <Register /> : <Login />}
+				</ConfigProvider>
 			</div>
 		</div>
 	);
+
+	function handleLoginClick() {
+		setIsActive(false);
+		navigate("/login", { replace: true });
+	}
+
+	function handleRegisterClick() {
+		setIsActive(true);
+		navigate("/signup", { replace: true });
+	}
 }
 
 export default Auth;
