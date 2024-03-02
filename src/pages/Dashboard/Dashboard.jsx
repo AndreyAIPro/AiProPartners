eb7ea7a1dc805d0e36975c9815e3a7b208c53361
TarasiykPavlo <kronusator909@gmail.com>
2024-03-02 22:02:41 +0200

    dashboard 95%

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,9 +1,116 @@
 import Plate from "../Preferences/Plate/Plate";
 import { ArrowDownOutlined, ArrowUpOutlined } from "@ant-design/icons";
-import { Statistic } from "antd";
+import { Button, Statistic } from "antd";
+import { Line } from "@ant-design/plots";
+import dayjs from "dayjs";
+import customParseFormat from "dayjs/plugin/customParseFormat";
+dayjs.extend(customParseFormat);
+const { RangePicker } = DatePicker;
+
+import { Table, Checkbox, ConfigProvider, theme, Select, DatePicker } from "antd";
+
 /** @format */
 
 const Charts = () => {
+    const config = {
+        data: {
+            type: "fetch",
+            value: "https://gw.alipayobjects.com/os/bmw-prod/55424a73-7cb8-4f79-b60d-3ab627ac5698.json",
+        },
+        xField: d => new Date(d.year),
+        yField: "value",
+        sizeField: "5",
+        shapeField: "trail",
+        theme: "classicDark",
+        legend: { size: false },
+        colorField: "category",
+    };
+    console.log(config.data.value);
+    const handleChange = value => {
+        console.log(`selected ${value}`);
+    };
+    const statisticAll = [{ value: "Общая статистика", label: "Общая статистика" }];
+    const periodType = [{ value: "Тип периода", label: "Тип периода" }];
+    const countries = [
+        { value: "Украина", label: "Украина" },
+        { value: "Омерика", label: "Омерика" },
+        { value: "Чайна", label: "Чайна" },
+        { value: "Пидорашка", label: "Пидорашка", disabled: true },
+    ];
+    const subAccounts = [
+        { value: "Вася", label: "Вася" },
+        { value: "Петя", label: "Петя" },
+        { value: "Саня", label: "Саня" },
+        { value: "Ваня", label: "Ваня", disabled: true },
+    ];
+    const ofers = [{ value: "Офер", label: "Офер" }];
+    const dataSource = [
+        {
+            key: "1",
+            date: "1/1/1900",
+            Unics: "2",
+            regestrationCount: "30%",
+            submitedRegestration: "2",
+            percentCTR: "3",
+            UO: "1",
+            percentUOS: "3",
+            percentCR2: "2",
+            incomeValue: "over9000",
+        },
+    ];
+    const columns = [
+        {
+            title: "Дата",
+            dataIndex: "date",
+            key: "date",
+        },
+        {
+            title: "Уники",
+            dataIndex: "Unics",
+            key: "Unics",
+        },
+        {
+            title: "Регистрации",
+            dataIndex: "regestrationCount",
+            key: "regestrationCount",
+        },
+        {
+            title: "Подтв. рег.",
+            dataIndex: "submitedRegestration",
+            key: "submitedRegestration",
+        },
+        {
+            title: "CTR, %",
+            dataIndex: "percentCTR",
+            key: "percentCTR",
+        },
+        {
+            title: "UO",
+            dataIndex: "UO",
+            key: "UO",
+        },
+        {
+            title: "UOS, %",
+            dataIndex: "percentUOS",
+            key: "percentUOS",
+        },
+        {
+            title: "CR2, %",
+            dataIndex: "percentCR2",
+            key: "percentCR2",
+        },
+        {
+            title: "Доход, $",
+            dataIndex: "incomeValue",
+            key: "incomeValue",
+        },
+    ];
+
+    // eslint-disable-next-line arrow-body-style
+    const disabledDate = current => {
+        // Can not select days before today and today
+        return current && current < dayjs().endOf("day");
+    };
     return (
         <>
             <div className="flex-1">
@@ -136,6 +243,182 @@ const Charts = () => {
                                 </div>
                             </div>
                         </Plate>
+                    </div>
+                </div>
+                <div className="flex-1 flex-col flex items-center ">
+                    <div className="dashboard-size-helper ">
+                        <ConfigProvider
+                            theme={{
+                                algorithm: theme.darkAlgorithm,
+                                components: {
+                                    Checkbox: {
+                                        colorPrimary: "#24A1E0",
+                                        colorBgContainer: "white",
+                                        colorBgBase: "white",
+                                        colorText: "white",
+                                        fontSize: 12,
+                                    },
+                                    Line: {},
+                                    Select: {
+                                        colorBorder: "white",
+                                        colorBgContainer: "black",
+                                    },
+                                    Button: {
+                                        colorBorder: "#24A1E0",
+                                    },
+                                    DatePicker: {
+                                        cellActiveWithRangeBg: "#24A1E0",
+                                    },
+                                    Icon: { color: "white" },
+                                },
+                            }}
+                        >
+                            <Plate
+                                title={
+                                    <div className="flex-nowrap flex ">
+                                        <div>Подробные данные статистики</div>
+                                        <div className="flex-nowrap flex ">
+                                            <div className="px-3">
+                                                <Select
+                                                    defaultValue="Общая статистика"
+                                                    onChange={handleChange}
+                                                    options={[...statisticAll]}
+                                                    size="small"
+                                                />
+                                            </div>
+                                            <div>
+                                                <Select
+                                                    defaultValue="Тип периода"
+                                                    onChange={handleChange}
+                                                    options={[...periodType]}
+                                                    size="small"
+                                                />
+                                            </div>
+                                        </div>
+                                    </div>
+                                }
+                            >
+                                <div className="flex flex-nowrap pb-3">
+                                    <div className="pr-5">
+                                        <Select
+                                            defaultValue="Страна"
+                                            onChange={handleChange}
+                                            options={[...countries]}
+                                            size="small"
+                                        />
+                                    </div>
+                                    <div className="pr-5">
+                                        <Select
+                                            defaultValue="Субаккаунт"
+                                            onChange={handleChange}
+                                            options={[...subAccounts]}
+                                            size="small"
+                                        />
+                                    </div>
+                                    <div className="pr-5">
+                                        <Select
+                                            defaultValue="Офер"
+                                            onChange={handleChange}
+                                            options={[...ofers]}
+                                            size="small"
+                                        />
+                                    </div>
+                                    <div className="pl-6">
+                                        <Button
+                                            size="small"
+                                            shape="round"
+                                        >
+                                            Обновить
+                                        </Button>
+                                    </div>
+                                </div>
+                                <div>
+                                    <Line {...config} />
+                                    <div className="px-2 py-2">
+                                        <Checkbox className="pr-4">Уники</Checkbox>
+                                        <Checkbox className="pr-4">Регистрации</Checkbox>
+                                        <Checkbox className="pr-4">Подтвержденные рег.</Checkbox>
+                                        <Checkbox className="pr-4">CTR, %</Checkbox>
+                                        <Checkbox className="pr-4">Уникальные покупки</Checkbox>
+                                        <Checkbox className="pr-4">Уникальные покупки в $</Checkbox>
+                                        <Checkbox className="pr-4">CR2, %</Checkbox>
+                                        <Checkbox className="pr-4">Доход, $</Checkbox>
+                                    </div>
+                                </div>
+                            </Plate>
+                            <Plate
+                                title={
+                                    <div className="flex-nowrap flex py-1 align-middle h-fit">
+                                        <div>Подробные данные статистики</div>
+                                        <div className="flex-nowrap flex ">
+                                            <div className="px-3 ">
+                                                <Select
+                                                    defaultValue="Общая статистика"
+                                                    onChange={handleChange}
+                                                    options={[...statisticAll]}
+                                                />
+                                            </div>
+                                            <div className="flex flex-nowrap flex-row text-text4 border-[1px] pl-2   rounded-md ">
+                                                <div className="flex justify-center  items-center pr-2">Выбрать диапазон дат</div>
+                                                <div className=" flex">
+                                                    {/* <ConfigProvider theme={{ algorithm: theme.defaultAlgorithm }}> */}
+                                                    <RangePicker
+                                                        disabledDate={disabledDate}
+                                                        size="small"
+                                                        variant={false}
+                                                        placeholder={["Начало", "Конец"]}
+                                                    />
+                                                    {/* </ConfigProvider> */}
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                }
+                            >
+                                <div>
+                                    <div className="flex flex-nowrap pb-3">
+                                        <div className="pr-5">
+                                            <Select
+                                                defaultValue="Страна"
+                                                onChange={handleChange}
+                                                options={[...countries]}
+                                                size="small"
+                                            />
+                                        </div>
+                                        <div className="pr-5">
+                                            <Select
+                                                defaultValue="Субаккаунт"
+                                                onChange={handleChange}
+                                                options={[...subAccounts]}
+                                                size="small"
+                                            />
+                                        </div>
+                                        <div className="pr-5">
+                                            <Select
+                                                defaultValue="Офер"
+                                                onChange={handleChange}
+                                                options={[...ofers]}
+                                                size="small"
+                                            />
+                                        </div>
+                                        <div className="pl-6">
+                                            <Button
+                                                size="small"
+                                                shape="round"
+                                            >
+                                                Обновить
+                                            </Button>
+                                        </div>
+                                    </div>
+                                </div>
+                                <Table
+                                    dataSource={dataSource}
+                                    columns={columns}
+                                    size="small"
+                                    pagination={false}
+                                ></Table>
+                            </Plate>
+                        </ConfigProvider>
                     </div>
                 </div>
             </div>
--- src/pages/Preferences/Plate/Plate.jsx
+++ src/pages/Preferences/Plate/Plate.jsx
@@ -2,8 +2,8 @@ export default function Plate({ title, children }) {
     return (
         <div className=" bg-secondary    flex-1  rounded-lg my-4  ">
             <div className=" rounded-lg px-2 w-full">
-                <div className="flex justify-between items-center ">
-                    <h2 className="flex text-title3 font-semibold  justify-center py-3">{title}</h2>
+                <div className="flex justify-between items-center w-full">
+                    <div className="flex text-title3 font-semibold   py-3 w-full">{title}</div>
                 </div>
                 <div>{children}</div>
             </div>
