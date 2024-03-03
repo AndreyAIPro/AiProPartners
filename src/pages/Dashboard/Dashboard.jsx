6ae798617a45d269345c39ef22765d421c9a0496
Roman Tsekhmistro <romantsdev@gmail.com>
2024-03-04 01:20:39 +0200

    pull finances

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,7 +1,7 @@
 /** @format */
 
 const Charts = () => {
-	return <section className='w-full'>HELLO CHARTS!</section>;
+	return <section className="w-full">HELLO CHARTS!</section>;
 };
 
 export default Charts;
--- src/pages/Finances/Finances.jsx
+++ src/pages/Finances/Finances.jsx
@@ -1,9 +1,9 @@
 /**  @format */
-import { useState } from 'react';
-import Filter from './../../components/common/Filter/Filter';
-import TableFinance from './../../components/common/TableFinance.jsx/TableFinance';
-import { filters } from './../../data/filtersForFinances';
-import './finances.modal.css';
+import { useState } from "react";
+import Filter from "./../../components/common/Filter/Filter";
+import TableFinance from "./../../components/common/TableFinance.jsx/TableFinance";
+import { filters } from "./../../data/filtersForFinances";
+import "./finances.modal.css";
 
 const Finances = () => {
 	const [modal, setModal] = useState(false);
@@ -13,46 +13,46 @@ const Finances = () => {
 	};
 
 	return (
-		<section className='w-[1245px] px-20 py-8'>
-			<h2 className=' text-title font-nunito-sans font-bold'>Финансы</h2>
-			<div className='flex gap-10 text-title2 mt-5 '>
+		<section className="w-[1245px] px-20 py-8">
+			<h2 className=" font-nunito-sans text-title font-bold">Финансы</h2>
+			<div className="mt-5 flex gap-10 text-title2 ">
 				<button
 					onClick={toggleModal}
-					className='font-nunito-sans font-bold text-light-gray'
+					className="font-nunito-sans font-bold text-light-gray"
 				>
 					Вывод средств
 				</button>
 
-				<p className='font-bold font-nunito-sans'>Список транзакций</p>
+				<p className="font-nunito-sans font-bold">Список транзакций</p>
 			</div>
 
-			<div className='mt-16 mx-6 flex gap-10'>
+			<div className="mx-6 mt-16 flex gap-10">
 				<Filter filters={filters} />
-				<button className='font-nunito-sans border-2 rounded-full px-4 border-light-blue '>
+				<button className="rounded-full border-2 border-light-blue px-4 font-nunito-sans ">
 					Обновить
 				</button>
 			</div>
-			<div className='m-6'>
+			<div className="m-6">
 				<TableFinance />
 			</div>
-			<div className='text-center'>
-				<button className='bg-light-blue rounded-full py-2 px-10 font-nunito-sans'>
+			<div className="text-center">
+				<button className="rounded-full bg-light-blue px-10 py-2 font-nunito-sans">
 					Загрузить ещё
 				</button>
 			</div>
 			{modal && (
 				<div>
-					<div onClick={toggleModal} className='overlay'></div>
-					<div className='modal-content bg-gray   w-[800px] h-[300px] rounded-xl flex flex-col justify-center items-center gap-4'>
-						<h2 className='text-title font-nunito-sans font-bold'>
+					<div onClick={toggleModal} className="overlay"></div>
+					<div className="modal-content flex   h-[300px] w-[800px] flex-col items-center justify-center gap-4 rounded-xl bg-gray">
+						<h2 className="font-nunito-sans text-title font-bold">
 							Подтвердите вывод
 						</h2>
-						<div className='flex justify-between px-2 '>
-							<button className='bg-light-blue rounded-full px-14 py-2 mr-10 font-nunito-sans text-text1'>
+						<div className="flex justify-between px-2 ">
+							<button className="mr-10 rounded-full bg-light-blue px-14 py-2 font-nunito-sans text-text1">
 								Подтверждаю
 							</button>
 							<button
-								className='bg-light-blue rounded-full px-8 py-2 font-nunito-sans text-text1'
+								className="rounded-full bg-light-blue px-8 py-2 font-nunito-sans text-text1"
 								onClick={toggleModal}
 							>
 								Нет
