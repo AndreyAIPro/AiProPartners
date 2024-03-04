a96eb85048f4d0e5b73bf946460c3ec067d054be
TarasiykPavlo <kronusator909@gmail.com>
2024-03-04 12:30:10 +0200

    SideBar scroll fix

--- src/components/Sidebar/LeftSidebar/LeftSidebar.jsx
+++ src/components/Sidebar/LeftSidebar/LeftSidebar.jsx
@@ -1,19 +1,21 @@
 /** @format */
 
-import TelegramLink from '../../common/TelegramLink';
-import SidebarNavMenu from '../../SidebarNavMenu';
-import SocialLinksList from '../../SocialLinksList';
-import LeftSidebarHeader from './LeftSidebarHeader';
+import TelegramLink from "../../common/TelegramLink";
+import SidebarNavMenu from "../../SidebarNavMenu";
+import SocialLinksList from "../../SocialLinksList";
+import LeftSidebarHeader from "./LeftSidebarHeader";
 
 const LeftSidebar = () => {
-	return (
-		<>
-			<LeftSidebarHeader />
-			<SidebarNavMenu />
-			<TelegramLink />
-			<SocialLinksList />
-		</>
-	);
+    return (
+        <>
+            <div className="sticky top-0">
+                <LeftSidebarHeader />
+                <SidebarNavMenu />
+                <TelegramLink />
+                <SocialLinksList />
+            </div>
+        </>
+    );
 };
 
 export default LeftSidebar;
