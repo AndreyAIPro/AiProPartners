8fd907bde313262a2e42ce9060f43a5df18026ef
KripakDebug <kastrula123@gmail.com>
2024-03-10 22:54:54 +0200

    add signup data

--- src/utils/supabaseUtils.js
+++ src/utils/supabaseUtils.js
@@ -25,6 +25,9 @@ export async function SignUp({
 				telegramAccount,
 				sources: radioButtonSources,
 				other: textAreaOtherRadio,
+				roles: ['partner'],
+				money: 0,
+				income: 0,
 			},
 		},
 	});
