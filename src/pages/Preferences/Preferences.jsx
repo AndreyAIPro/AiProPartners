755b715df3013f01ff07d2e2d6dd38e67aa03303
KripakDebug <kastrula123@gmail.com>
2024-03-11 17:51:11 +0200

    fix merge in Pavlo

--- src/pages/Preferences/Preferences.jsx
+++ src/pages/Preferences/Preferences.jsx
@@ -18,7 +18,6 @@ import { useUser } from "../../hooks/useUser";
 import { useUpdateUser } from "../../hooks/useUpdateUser";
 
 const Preferences = () => {
-	const { user } = useUser();
 	const { updateUser } = useUpdateUser();
 	const { discordAccount, nickname, other, phone, sources, telegramAccount } =
 		user.user_metadata;
@@ -460,8 +459,7 @@ const Preferences = () => {
 				messageApi.open({
 					type: "success",
 					duration: 5,
-					content:
-						"Аккаунт оновленно!",
+					content: "Аккаунт оновленно!",
 				});
 			},
 			onError: () => {
