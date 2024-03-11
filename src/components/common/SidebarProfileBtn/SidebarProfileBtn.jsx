cea84f373df05d13b587577fb1efcc0c5a480245
TarasiykPavlo <kronusator909@gmail.com>
2024-03-11 13:39:10 +0200

    Exit icon +popConfirm

--- src/components/common/SidebarProfileBtn/SidebarProfileBtn.jsx
+++ src/components/common/SidebarProfileBtn/SidebarProfileBtn.jsx
@@ -1,31 +1,62 @@
 /** @format */
 
-import { NavLink } from "react-router-dom";
+import { useLinkClickHandler, NavLink } from "react-router-dom";
 import { ReactComponent as ProfilePlaceholder } from "../../../assets/images/profile-placeholder.svg";
 import { ReactComponent as SettingsIcon } from "../../../assets/images/settings-btn.svg";
 import styles from "./sidebar-profile-btn.module.scss";
+import { ReactComponent as ExitIcon } from "../../../assets/images/Icons aipro partners/exit.svg";
+import { Popconfirm, ConfigProvider, theme, Button } from "antd";
 
 const SidebarProfileBtn = () => {
-    return (
-        <div className={styles.btn}>
-            <div className={styles.btn__profile}>
-                <ProfilePlaceholder />
-                <h3>Имя Фамилия</h3>
-            </div>
-            <NavLink
-                to="/preferences"
-                className={styles.settings__btn}
-                style={({ isActive }) => {
-                    return {
-                        backgroundColor: isActive ? "#24a1e0" : "",
-                        borderRadius: "15px",
-                    };
-                }}
-            >
-                <SettingsIcon />
-            </NavLink>
-        </div>
-    );
+	const handleConfirm = () => {
+		console.log(11111111111);
+	};
+	return (
+		<div className={styles.btn}>
+			<div className={styles.btn__profile}>
+				<ProfilePlaceholder />
+				<h3>Имя Фамилия</h3>
+			</div>
+			<div className=" flex flex-row items-center justify-center ">
+				<ConfigProvider
+					theme={{
+						algorithm: theme.darkAlgorithm,
+					}}
+				>
+					<Popconfirm
+						title="Хотите выйти ?"
+						onConfirm={useLinkClickHandler("/")}
+						okText="Да"
+						cancelText="Нет"
+						placement="left"
+					>
+						<Button
+							style={{
+								paddingLeft: " 2px",
+								paddingRight: "2px",
+								borderColor: "#0e0e0e",
+							}}
+						>
+							<ExitIcon />
+						</Button>
+					</Popconfirm>
+				</ConfigProvider>
+
+				<NavLink
+					to="/preferences"
+					className={styles.settings__btn}
+					style={({ isActive }) => {
+						return {
+							backgroundColor: isActive ? "#24a1e0" : "",
+							borderRadius: "15px",
+						};
+					}}
+				>
+					<SettingsIcon />
+				</NavLink>
+			</div>
+		</div>
+	);
 };
 
 export default SidebarProfileBtn;
